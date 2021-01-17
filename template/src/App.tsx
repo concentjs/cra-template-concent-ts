a75429ee0512f2218a0ad654e1f8300a90e5a33e
fancyzhong <fancyzhong@tencent.com>
2021-01-17 21:03:26 +0800

    refactor: mod App.test

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,3 +1,4 @@
+import 'configs/runConcent';
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from 'features/counter/Counter';
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'configs/runConcent';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
